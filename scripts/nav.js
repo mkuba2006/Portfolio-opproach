cd6599bb4c14c6d0f3512426737003653b8ed405
Jakub Myrta <mkuba2006@gmail.com>
2023-05-17 23:11:20 +0200

    17

--- scripts/nav.js
+++ scripts/nav.js
@@ -18,9 +18,6 @@ burger.addEventListener("click", () => {
   nav_links.forEach((link, index) => {
     link.style.animation = `navLink 0.5s ease forwards ${index / 7}s`;
   });
-  document.getElementById("jeden").style.transform ='rotate(-45deg) translate(-9px, 4px)';
-  document.getElementById("dwa").style.opacity = '0';
-  document.getElementById("trzy").style.transform = 'rotate(45deg) translate(-9px, -4px)';
 });
 
 const hrefs = document.querySelectorAll("#secret a");
@@ -29,8 +26,6 @@ const secretDiv = document.getElementById('secret');
 hrefs.forEach(href=>{
   href.addEventListener('click', ()=>{
     secretDiv.classList.remove('nav_active');
-    document.getElementById("jeden").style.transform ='rotate(0deg) translate(0px, 0px)';
-    document.getElementById("dwa").style.opacity = '100';
-    document.getElementById("trzy").style.transform = 'rotate(0deg) translate(0px, 0px)';
+    burger.classList.toggle("toggle");
   })
 })
